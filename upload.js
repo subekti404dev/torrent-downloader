ca09b27790d75ad44b8a84a760e5a0df1e8a3be7
Urip Subekti <imanuel.subekti@efishery.com>
2022-06-22 23:24:37 +0700

    fix: add parent driveI id as an argument

--- upload.js
+++ upload.js
@@ -1,33 +1,33 @@
-require('dotenv').config();
-const { getDrive, createDir, uploadFile } = require('./drive');
-const fs = require('fs');
+require("dotenv").config();
+const { getDrive, createDir, uploadFile } = require("./drive");
+const fs = require("fs");
 
 const walk = async (dir, driveDirId) => {
-    const drive = getDrive();
-    const list = fs.readdirSync(dir);
-    for (const file of list) {
-        const filepath = dir + '/' + file;
-        const stat = fs.statSync(filepath);
-        if (stat && stat.isDirectory()) {
-            console.log('Create Dir: ', filepath);
-            const newDir = await createDir(drive, file, driveDirId);
-            walk(filepath, newDir.id);
-        } else {
-            console.log('Upload File: ', filepath);
-            await uploadFile(drive, filepath, file, driveDirId);
-        }
+  const drive = getDrive();
+  const list = fs.readdirSync(dir);
+  for (const file of list) {
+    const filepath = dir + "/" + file;
+    const stat = fs.statSync(filepath);
+    if (stat && stat.isDirectory()) {
+      console.log("Create Dir: ", filepath);
+      const newDir = await createDir(drive, file, driveDirId);
+      walk(filepath, newDir.id);
+    } else {
+      console.log("Upload File: ", filepath);
+      await uploadFile(drive, filepath, file, driveDirId);
     }
-}
+  }
+};
 
 const main = async () => {
-    try {
-        const filepath = '/data';
-        console.log({ filepath });
-        const driveParentId = process.env.GA_TMP_DRIVE_PARENT_ID;
-        await walk(filepath, driveParentId);
-    } catch (error) {
-        console.log(error?.response?.data || error?.message);
-    }
-}
+  try {
+    const filepath = "/data";
+    console.log({ filepath });
+    const driveParentId = process.argv[2] || process.env.GA_TMP_DRIVE_PARENT_ID;
+    await walk(filepath, driveParentId);
+  } catch (error) {
+    console.log(error?.response?.data || error?.message);
+  }
+};
 
-main()
+main();
